2b99887d43b49fdc77504c356919ab638167caab
attaluris <sritej@gmail.com>
2021-02-22 22:18:06 -0800

    add bottom bar everywhere except events

--- components/bottom_bar.js
+++ components/bottom_bar.js
@@ -1,70 +1,51 @@
-class topLevelHeader extends HTMLElement {
+class bottomBar extends HTMLElement {
     constructor() {
       super();
     }
   
     connectedCallback() {
       this.innerHTML = `
-            <div class="bignav">
-            <table>
-                <tbody>
-                    <tr>
-                        <td><a href="../index.html">Home</a></td>
-                        <td><a href="../aboutus.html">About Us</a></td>
-                        <td>
-                            <div class="dropdown">
-                                <span class="normal">
-                                    <a href="../workshop/index.html">Events</a>
-                                </span>
-                                <div class="dropdown-content">
-                                    <p class="menu"><a href="../workshop/MD4SG20/index.html">MD4SG &#39;20</a></p>
-                                    <p class="menu"><a href="../workshop/DSA20/dsa20tutorial.html">DSA &#39;20</a></p>
-                                    <p class="menu"><a href="../workshop/EC20/ec20tutorial.html">EC &#39;20</a></p>
-                                    <p class="menu"><a href="../workshop/compass19.html">COMPASS &#39;19</a></p>
-                                    <p class="menu"><a href="../workshop/EC19/index.html">MD4SG &#39;19</a></p>
-                                    <p class="menu"><a href="../workshop/EC18/index.html">MD4SG &#39;18</a></p>
-                                    <p class="menu"><a href="../workshop/winetutorial17.html">WINE &#39;17</a></p>
-                                    <p class="menu"><a href="../workshop/EC17/index.html">MD4SG &#39;17</a></p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="dropdown">
-                                <span class="normal">
-                                    <a href="../colloquium/index.html">Colloquium Series</a>
-                                </span>
-                                <div class="dropdown-content">
-                                    <p class="menu"><a href="../colloquium/index.html">Upcoming</a></p>
-                                    <p class="menu"><a href="../colloquium/past.html">Previous </a></p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="dropdown">
-                                <span class="normal">
-                                    <a href="../workinggroups/index.html"><b>Working Groups</b></a>
-                                </span>
-                                <div class="dropdown-content">
-                                    <p class="menu"><a href="../workinggroups/dev.html">Development</a></p>
-                                    <p class="menu"><a href="../workinggroups/environment.html">Environment</a></p>
-                                    <p class="menu"><a href="../workinggroups/fairness.html">Fairness</a></p>
-                                    <p class="menu"><a href="../workinggroups/data.html">Data Economies</a></p>
-                                    <p class="menu"><a href="../workinggroups/inequality.html">Inequality</a></p>
-                                    <p class="menu"><a href="../workinggroups/civicparticipation.html">Civic Participation</a></p>
-                                    <p class="menu"><a href="../workinggroups/asiapacific.html">Asia-Pacific</a></p>
-                                    <p class="menu"><a href="../workinggroups/lawpolicy.html">Law & Policy</a></p>
-                                    <p class="menu"><a href="../workinggroups/distributivejustice.html">Discrimination in Algorithmic Decision-making</a></p>
-                                    <p class="menu"><a href="../workinggroups/latinamerica.html">Latin America and Caribbean</a></p>
-                                    <p class="menu"><a href="../workinggroups/previous.html">Previous Working Groups</a></p>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="bottom-bar">
+            <div class="bottom-bar-category">
+                <strong>Organizers:</strong> <a href="http://www.cs.cornell.edu/~red/" target="_blank">Rediet Abebe</a>, <a href="https://web.stanford.edu/~wanyili/">Wanyi Li</a>,
+                <a href="https://sites.google.com/view/irene-lo" target="_blank">Irene Lo</a>, <a href="https://www.fmarmolejo.com/">Francisco Marmolejo Cossio</a>, and <a href="http://www.columbia.edu/~as5001/">Ana-Andreea Stoica</a>.
+            </div>
+            <div class="bottom-bar-category">
+                <strong>Contact:</strong> <a href="mailto:organizers@md4sg.com">organizers@md4sg.com</a>
+            </div>
+            <div class="bottom-bar-category">
+                <strong>Join us:</strong> <a href="https://docs.google.com/forms/d/e/1FAIpQLSfc2uwgKLw8Vfceq4l0UvuzxUGJcyf5U7Vc1mYwNR1yi3UYqA/viewform?usp=sf_link">MD4SG email list</a>
+            </div>
+            <div class="bottom-bar-category">
+                <strong>Subscribe:</strong> <a href="https://www.youtube.com/channel/UC2hBoxmzDtAwTTjRRM99f4w/videos">Youtube channel</a>,
+                <a href="https://calendar.google.com/calendar/r?cid=dTZqdnE1dW9pNjdiaXE2bGlvZ3Zkb2RoM2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">Calendar</a>,
+                <a href="https://www.google.com/url?q=https://medium.com/@md4sg&amp;sa=D&amp;source=hangouts&amp;ust=1589249244225000&amp;usg=AFQjCNGPKdq4H0b4o-GyDXXO1IV8dxYUww">Medium</a>, <a href="https://twitter.com/MD4SG">Twitter</a>	
+            </div>
+
+            <div class="footer">
+                <small>Updated Feb 2021. Web design by <a href="http://www.kiragoldner.com/">Kira Goldner</a> + <a href="http://www.sritej.com/">Sritej Attaluri</a>.</small>
+                <script>
+                    (function (i, s, o, g, r, a, m) {
+                        i["GoogleAnalyticsObject"] = r;
+                        (i[r] =
+                            i[r] ||
+                            function () {
+                                (i[r].q = i[r].q || []).push(arguments);
+                            }),
+                            (i[r].l = 1 * new Date());
+                        (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+                        a.async = 1;
+                        a.src = g;
+                        m.parentNode.insertBefore(a, m);
+                    })(window, document, "script", "../www.google-analytics.com/analytics.js", "ga");
+
+                    ga("create", "UA-93664120-1", "auto");
+                    ga("send", "pageview");
+                </script>
+            </div>
         </div>
       `;
     }
   }
   
-  customElements.define('top-level-header-component', topLevelHeader);
+  customElements.define('bottom-bar-component', bottomBar);
