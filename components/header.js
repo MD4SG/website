e069327462f729ea49f133e1711600026606a603
attaluris <sritej@gmail.com>
2021-03-01 16:45:59 -0800

    add bios and dropdowns update and index

--- components/header.js
+++ components/header.js
@@ -17,6 +17,7 @@ class topLevelHeaderDepthZero extends HTMLElement {
                                     <a href="./workshop/index.html">Events</a>
                                 </span>
                                 <div class="dropdown-content">
+                                    <p class="menu"><a href="./workshop/FAACT21/faact21tutorial.html">FAccT &#39;21</a></p>
                                     <p class="menu"><a href="./workshop/MD4SG20/index.html">MD4SG &#39;20</a></p>
                                     <p class="menu"><a href="./workshop/DSA20/dsa20tutorial.html">DSA &#39;20</a></p>
                                     <p class="menu"><a href="./workshop/EC20/ec20tutorial.html">EC &#39;20</a></p>
@@ -86,6 +87,7 @@ class topLevelHeaderDepthOne extends HTMLElement {
                                     <a href="../workshop/index.html">Events</a>
                                 </span>
                                 <div class="dropdown-content">
+                                    <p class="menu"><a href="../workshop/FAACT21/faact21tutorial.html">FAccT &#39;21</a></p>
                                     <p class="menu"><a href="../workshop/MD4SG20/index.html">MD4SG &#39;20</a></p>
                                     <p class="menu"><a href="../workshop/DSA20/dsa20tutorial.html">DSA &#39;20</a></p>
                                     <p class="menu"><a href="../workshop/EC20/ec20tutorial.html">EC &#39;20</a></p>
@@ -155,6 +157,7 @@ class topLevelHeaderDepthOne extends HTMLElement {
                                     <a href="../../workshop/index.html">Events</a>
                                 </span>
                                 <div class="dropdown-content">
+                                    <p class="menu"><a href="../../workshop/FAACT21/faact21tutorial.html">FAccT &#39;21</a></p>
                                     <p class="menu"><a href="../../workshop/MD4SG20/index.html">MD4SG &#39;20</a></p>
                                     <p class="menu"><a href="../../workshop/DSA20/dsa20tutorial.html">DSA &#39;20</a></p>
                                     <p class="menu"><a href="../../workshop/EC20/ec20tutorial.html">EC &#39;20</a></p>
