d0bf217a72ae17f5ef3abe650262dabb72a4605e
attaluris <sritej@gmail.com>
2021-06-21 18:04:31 -0700

    add linkedin

--- components/bottom_bar.js
+++ components/bottom_bar.js
@@ -31,8 +31,19 @@ class bottomBar extends HTMLElement {
                                 <li><a href="https://forms.gle/yq8wbrv3LzhRfLjz7">Mailing List</a></li>
                             </ul>
                         </div>
-                        <div class="col-lg-3 item social"><a href="https://twitter.com/MD4SG"><i class="icon ion-social-twitter"></i></a><a href="https://www.youtube.com/channel/UC2hBoxmzDtAwTTjRRM99f4w/videos"><i class="icon ion-social-youtube"></i></a><a href="mailto:organizers@md4sg.com"><i class="icon ion-email"></i></a><a href="https://calendar.google.com/calendar/r?cid=dTZqdnE1dW9pNjdiaXE2bGlvZ3Zkb2RoM2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"><i class="icon ion-android-calendar"></i></a><a href="https://medium.com/@md4sg"><i class="icon ion-social-markdown"></i></a>
-                            <p class="copyright">Updated March 2021.  Website design by:<br>Kira Goldner + <a href="https://sritej.com/">Sritej Attaluri</a> + <br><a href="https://georgeobaido.com/">George Obaido</a></p>
+                        <div class="col-lg-3 item social">
+                            <a href="https://twitter.com/MD4SG"><i class="icon ion-social-twitter"></i></a>
+                            <a href="https://www.linkedin.com/company/md4sg/"><i class="icon ion-social-linkedin"></i></a>
+                            <a href="https://www.youtube.com/channel/UC2hBoxmzDtAwTTjRRM99f4w/videos"><i class="icon ion-social-youtube"></i></a>
+                            <a href="mailto:organizers@md4sg.com"><i class="icon ion-email"></i></a>
+                            <a href="https://calendar.google.com/calendar/r?cid=dTZqdnE1dW9pNjdiaXE2bGlvZ3Zkb2RoM2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"><i class="icon ion-android-calendar"></i></a>
+                            <a href="https://medium.com/@md4sg"><i class="icon ion-social-markdown"></i></a>
+                            <p class="copyright">
+                                Updated March 2021. Website design by:<br />
+                                Kira Goldner<br />
+                                <a href="https://sritej.com/">Sritej Attaluri</a><br />
+                                <a href="https://georgeobaido.com/">George Obaido</a>
+                            </p>
                         </div>
                     </div>
                 </div>
